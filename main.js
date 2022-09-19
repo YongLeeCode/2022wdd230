2ba1cb5f2a22e9ea7f99d52d2eed773410c161a1
Yong <yonghwan311@gmail.com>
2022-09-19 11:10:19 -0600

    adding normalize.css

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ let currentdate = document.lastModified;
 document.querySelector('.year').textContent = year;
 console.log(currentdate);
 document.querySelector('.updated').textContent = currentdate;
+
+const fulldate1 = dayName + ", " + d.getDate() + ", "+ monthName + " " + year;
+const fulldate2= `${dayName}, ${d.getDate()}, ${monthName} ${year}`;
+const daysUntil= `There are ${daycount} days until ${targetEvent}!`;
