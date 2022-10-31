c3954d979cb52ba876ba74acf62c2f952f6be335
Yong <yonghwan311@gmail.com>
2022-10-30 23:21:11 -0600

    refresh

---
+++ lesson7/lazy.js
@@ -0,0 +1,32 @@
+const imagesToLoad = document.querySelectorAll("img[data-src]")
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {image.removeAttribute("data-src");};
+};
+
+if("IntersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
+}
